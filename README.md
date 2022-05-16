adb0533eb9fa11c96af0558fe101524eef14205d
coleenhuang <coleenhuang@gmail.com>
2022-05-15 20:16:12 -0700

    added logic to ensure that all fields of form were filled out before the form info is added to the library

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Library-App
+A simple library app that was done as part of the Odin project
+
+## Things I learned
+* Familiarizing myself with using constructors as a quicker and more efficient way to create objects and store data
--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const $form = document.querySelector("form").addEventListener("submit", (e) => {
     e.preventDefault();
     console.log('form submitted')
     addBooktoLibrary()
-    console.log(myLibrary)
-    console.log(myLibrary[0].title)
   });
 const $title = document.querySelector("#title");
 const $author = document.querySelector("#author");
@@ -21,9 +19,17 @@ function Book(title, author, status) {
 }
 
 function addBooktoLibrary() {
-    //take form input and add a new book
-    let newBook = new Book($title.value, $author.value, $status.value)
-    myLibrary.push(newBook);
+    //take form input and add a item to myLibrary array
+
+    if ($title.value.length === 0 || $author.value.length === 0) {
+        //form validation
+        alert('Please finish filling out all the fields')
+    }
+    else {
+        let newBook = new Book($title.value, $author.value, $status.value)
+        myLibrary.push(newBook);
+    }
+    
 }
 
 

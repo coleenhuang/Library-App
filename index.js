3297ed6bed633a27c0893038692f870e96d423ff
coleenhuang <coleenhuang@gmail.com>
2022-05-15 20:42:14 -0700

    added function to reset form inputs after submitting

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const $form = document.querySelector("form").addEventListener("submit", (e) => {
     e.preventDefault();
     console.log('form submitted')
     addBooktoLibrary()
+    document.querySelector('form').reset();
   });
 
 const $title = document.querySelector("#title");
